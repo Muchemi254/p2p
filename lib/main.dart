d65c2265ea59f6239c0019c69975fd39df54aedb
Muchemi <kimanimuchemi166@gmail.com>
2024-07-22 17:15:12 +0300

    button visibility

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,69 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
+void main() {
   runApp(
-    const MaterialApp(home: WelcomeScreen()
+    const MaterialApp(
+      home: WelcomeScreen(),
     ),
-     );
+  );
 }
 
-
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool showPhoneForm = true;
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    phoneController.dispose();
+    emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 30),
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [Colors.black, Colors.orange],
-            stops: [0.0, 10.0], // Optional: define stops for the gradient colors
-          ),
+          color: Colors.white
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             //logo
-            const FlutterLogo(size: 150,),
-            const SizedBox(height: 20,),
-
-
+            //const FlutterLogo(size: 150,),
+            const SizedBox(height: 20),
             Container(
-              margin: const EdgeInsets.symmetric(horizontal: 5.0), // Margin around the entire container
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
               child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0), // Padding inside the Text widget
-                child:  Text('Welcome to\n the world of LOOP!',
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Welcome to\n the world of LOOP!',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 24,
+                    fontSize: 30,
                     fontWeight: FontWeight.bold,
-                  ),),
+                  ),
+                ),
               ),
             ),
-
-            const SizedBox( height: 15,),
-
+            const SizedBox(height: 15),
             Container(
-              margin: const EdgeInsets.symmetric(horizontal: 5.0), // Margin around the entire container
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
               child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 0.0), // Padding inside the Text widget
+                padding: EdgeInsets.symmetric(horizontal: 0.0),
                 child: Text(
-                  'Please enter 33 your phone number\n to proceed',
+                  'Please enter your phone number\n to proceed',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 14,
@@ -64,158 +72,139 @@ class WelcomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-
-
-
-
-            const SizedBox( height: 25,),
-
-            Container(
-              padding: const EdgeInsets.all(5.0), // Padding around the child widget
-              decoration: BoxDecoration(
-                color: Colors.grey[200], // Background color of the container
-                borderRadius: BorderRadius.circular(10.0), // Border radius all around
-              ),
-              child: //Buttons
-              Row(children: <Widget>[
-                Expanded(child: ElevatedButton(
-                  onPressed:(){},// button logic
-                  child: const Text ('Mobile Phone'),
-                ),),
-                Expanded(child: ElevatedButton(
-                  onPressed:(){},// button logic
-                  child: const Text ('Email'),
-                ),),
-              ],),
-            ),
-
-
-
-
-
-
-
-
-            const SizedBox( height: 20,),
-
- Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200], // Background color of the container
-                borderRadius: BorderRadius.circular(10.0), // Border radius all around
-              ),
-            child:Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+            const SizedBox(height: 20),
+            // Login Form
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Kenya ',
-                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.normal),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      showPhoneForm = true;
+                    });
+                  },
+                  child: const Text("Phone"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      showPhoneForm = false;
+                    });
+                  },
+                  child: const Text("Email"),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Visibility(
+              visible: showPhoneForm,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15.0),
+                  border: Border.all( // Add this line for the border
+                    color: Colors.grey, // Set the border colorwidth: 1.0, // Set the border width
                   ),
                 ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(25.0),
+                      child: Text(
+                        '+254 ',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        width: 200,
+                        child: TextField(
+                          controller: phoneController,
+                          keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(
+                            hintText: 'Enter your phone number',
+                            hintStyle: const TextStyle(color: Colors.grey),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              borderSide: const BorderSide(color: Colors.grey),
+                            ),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Visibility(
+              visible: !showPhoneForm,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        width: 200,
+                        child: Padding(
+                          padding: const EdgeInsets.all(9.0),
+                          child: TextField(
+                            controller: emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              hintText: '   Enter your email address',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                borderSide: const BorderSide(color: Colors.white),
+                              ),
+                              enabledBorder: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            //Buttons
+            Row(
+              children: <Widget>[
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {
-                      
-                    },
-                    child: const Text(''),
-        
+                    onPressed: () {}, // button logic
+                    child: const Text('Sign In'),
                   ),
                 ),
               ],
             ),
-            ),
-
-   
- const SizedBox( height: 20,),
-
- Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200], // Background color of the container
-                borderRadius: BorderRadius.circular(10.0), // Border radius all around
-              ),
-            child:Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    '+254 ',
-                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.normal),
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your phone number',
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      border: OutlineInputBorder( // Create a plain box border
-                        borderRadius: BorderRadius.circular(8.0),
-                        borderSide: const BorderSide(color: Colors.white), // Optional border color
-                      ),
-                      enabledBorder: InputBorder.none, // Remove the default underline
-                      focusedBorder: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            ),
-
-const SizedBox(height: 20,),
-
- Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[200], // Background color of the container
-                borderRadius: BorderRadius.circular(10.0), // Border radius all around
-              ),
-            child:Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                      hintText: '   Enter your email address',
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      border: OutlineInputBorder( // Create a plain box border
-                        borderRadius: BorderRadius.circular(8.0),
-                        borderSide: const BorderSide(color: Colors.white), // Optional border color
-                      ),
-                      enabledBorder: InputBorder.none, // Remove the default underline
-                      focusedBorder: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            ),
-
-            const SizedBox(height: 20,),
-
+            const SizedBox(height: 20),
             //Buttons
-            Row(children: <Widget>[
-              Expanded(child: ElevatedButton(
-                onPressed:(){},// button logic
-                child: const Text ('sign in'),
-              ),),
-            ],),
-
-            const SizedBox(height: 20,),
-
-            //Buttons
-            Row(children: <Widget>[
-              Expanded(child: ElevatedButton(
-                onPressed:(){},// button logic
-                child: const Text ('Create an account'),
-              ),),
-            ],),
-
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {}, // button logic
+                    child: const Text('Create an account'),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
 }
-
